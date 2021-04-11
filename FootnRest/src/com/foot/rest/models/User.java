688951c2e85c5d475f8aea919117d59d7510a72b
imadhou <imadhou00@gmail.com>
2021-04-11 03:11:01 +0200

    initializing and setting up basic authentication

---
+++ FootnRest/src/com/foot/rest/models/User.java
@@ -0,0 +1,42 @@
+package com.foot.rest.models;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class User {
+	
+	private String name;
+	private String fname;
+	private String email;
+	private String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getEmail()+"::"+this.getPassword();
+	}
+
+}
