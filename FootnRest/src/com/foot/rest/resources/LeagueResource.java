7549be90917e3edb2b4b43638ff425e42e7acade
bapn3454 <73539312+bapn3454@users.noreply.github.com>
2021-04-15 18:10:30 +0100

    structure complétée .. to do : impl bdd

--- FootnRest/src/com/foot/rest/resources/LeagueResource.java
+++ FootnRest/src/com/foot/rest/resources/LeagueResource.java
@@ -1,13 +1,52 @@
 package com.foot.rest.resources;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.foot.rest.models.League;
+import com.foot.rest.services.LeagueService;
 
 
 
 @Path("/leagues")
 public class LeagueResource {
 	
+	private LeagueService leagueService = new LeagueService();
 	
+	@Path("/{id_league}")
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public League getLeagueById(@PathParam("id_league") String id_league ){
+		
+		return leagueService.getLeagueById(id_league);
+	}
 	
+	@Path("/")
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	public void createLeague(League league) {
+		leagueService.createLeague(league);
+	}
+	
+	@Path("/{id_league}")
+	@PUT
+	@Produces(MediaType.APPLICATION_XML)
+	public void updateLeague(@PathParam("id_league") String id_league, League league ){
+		leagueService.updateLeague(id_league,league);
+	}
+	
+	@Path("/{id_league}")
+	@DELETE
+	@Produces(MediaType.APPLICATION_XML)
+	public void deleteLeague(@PathParam("id_league") String id_league ){
+		leagueService.deleteLeague(id_league);
+	}
 
 }
---
+++ FootnRest/src/com/foot/rest/services/LeagueService.java
@@ -0,0 +1,27 @@
+package com.foot.rest.services;
+
+import com.foot.rest.models.League;
+
+public class LeagueService {
+
+	public League getLeagueById(String id_League) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void createLeague(League League) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateLeague(String id_League, League League) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteLeague(String id_League) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
